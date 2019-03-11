924f0d7f2ee5ab3bb8f97ee882582ec4328c21e3
MuellerSeb <mueller.seb@posteo.de>
2019-03-11 14:41:34 +0100

    blackened

--- examples/02_perform.py
+++ examples/02_perform.py
@@ -11,10 +11,12 @@ from pentapy import tools
 from scipy.linalg.lapack import dgbsv
 import perfplot
 
+
 def get_les(size):
     mat = (np.random.random((5, size)) - 0.5) * 1e-5
     V = np.array(np.random.random(size) * 1e5)
     return mat, V
+
 
 def solve_1(in_val):
     mat, V = in_val
@@ -22,29 +24,35 @@ def solve_1(in_val):
     M = sps.spdiags(mat, [2, 1, 0, -1, -2], size, size, format="csc")
     return spsolve(M, V, use_umfpack=False)
 
+
 def solve_2(in_val):
     mat, V = in_val
     size = mat.shape[1]
     M = sps.spdiags(mat, [2, 1, 0, -1, -2], size, size, format="csc")
     return spsolve(M, V, use_umfpack=True)
 
+
 def solve_3(in_val):
     mat, V = in_val
     M = tools.shift_banded(mat, col_to_row=True)
     return ps1(M, V)
 
+
 def solve_4(in_val):
     mat, V = in_val
     return ps2(mat, V, is_flat=True, index_row_wise=False)
 
+
 def solve_5(in_val):
     mat, V = in_val
-    M= np.vstack((np.zeros((2, mat.shape[1])), mat))
+    M = np.vstack((np.zeros((2, mat.shape[1])), mat))
     return dgbsv(2, 2, M, V)[2]
+
 
 def solve_6(in_val):
     mat, V = in_val
     return solve_banded((2, 2), mat, V)
+
 
 def solve_7(in_val):
     mat, V = in_val
@@ -54,25 +62,17 @@ def solve_7(in_val):
 
 perfplot.show(
     setup=get_les,
-    kernels=[
-        solve_1,
-        solve_2,
-        solve_3,
-        solve_4,
-        solve_5,
-        solve_6,
-        solve_7,
-    ],
+    kernels=[solve_1, solve_2, solve_3, solve_4, solve_5, solve_6, solve_7],
     labels=[
-        'scipy sparse',
-        'umfpack',
-        'penta_py',
-        'penta_cy',
-        'lapack dgbsv',
-        'scipy solve_banded',
-        'numpy',
+        "scipy sparse",
+        "umfpack",
+        "penta_py",
+        "penta_cy",
+        "lapack dgbsv",
+        "scipy solve_banded",
+        "numpy",
     ],
-    n_range=[2**k for k in range(2, 13)],
-    xlabel='Size [n]',
+    n_range=[2 ** k for k in range(2, 13)],
+    xlabel="Size [n]",
     logy=True,
 )
