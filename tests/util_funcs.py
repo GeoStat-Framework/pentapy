ae656cf94f38b4651545161e848443a97930b53f
MothNik <nik.zoe@web.de>
2024-06-08 11:13:44 +0200

    test: [11] created conditioned banded matrix creator for testing purposes

--- tests/util_funcs.py
+++ tests/util_funcs.py
@@ -9,8 +9,14 @@ from functools import partial
 from typing import Tuple
 
 import numpy as np
+from scipy import sparse as sprs
 
 import pentapy as pp
+
+# === Constants ===
+
+_MIN_DIAG_VAL = 1e-3
+
 
 # === Utility Functions ===
 
@@ -26,7 +32,7 @@ def get_diag_indices(
     https://stackoverflow.com/a/18081653/14814813
 
     Doctests
-    ======--
+    --------
     >>> # Setting up a test matrix
     >>> n_rows = 5
     >>> mat = np.arange(start=0, stop=n_rows * n_rows).reshape(n_rows, n_rows)
@@ -111,7 +117,7 @@ def gen_rand_penta_matrix_dense_int(
     data type ``int64``.
 
     Doctests
-    ======--
+    --------
     >>> # Generating a random pentadiagonal matrix with NumPy indices
     >>> n_rows = 5
     >>> seed = 19_031_977
@@ -166,6 +172,195 @@ def gen_rand_penta_matrix_dense_int(
         mat[row_idxs, col_idxs] = gen_rand_int(size=n_rows - abs(offset))
 
     return mat
+
+
+def gen_conditioned_rand_penta_matrix_dense(
+    n_rows: int,
+    seed: int,
+    ill_conditioned: bool,
+) -> np.ndarray:
+    """
+    Generates a well- or ill-conditioned random banded pentadiagonal matrix with shape
+    ``(n_rows, n_rows)``.
+
+    This is achieved as follows:
+    - a fake LDU decomposition is generated where ``L`` and ``U`` are unit lower and
+      upper triangular matrices, respectively, and ``D`` is a diagonal matrix
+    - the matrix is then reconstructed by multiplying the three matrices and converting
+      the result to a banded matrix
+
+    If ``D`` does not have any zeros or values of small magnitude compared to the
+    largest value, the matrix should be well-conditioned.
+    Otherwise, it is ill-conditioned.
+
+    Doctests
+    --------
+    >>> # Imports
+    >>> from scipy.linalg import bandwidth
+
+    >>> # 1) Generating a super small well-conditioned random pentadiagonal matrix
+    >>> n_rows = 3
+    >>> seed = 19_031_977
+
+    >>> mat = gen_conditioned_rand_penta_matrix_dense(
+    ...     n_rows=n_rows,
+    ...     seed=seed,
+    ...     ill_conditioned=False,
+    ... )
+    >>> mat
+    array([[ 0.92453713,  0.28308514, -0.09972199],
+           [-0.09784268,  0.2270634 , -0.1509019 ],
+           [-0.23431267,  0.00468463,  0.22991003]])
+    >>> # its bandwidth is computed and should be equal to 2
+    >>> bandwidth(mat)
+    (2, 2)
+    >>> # its condition number is computed and values below 1e10 can be considered good
+    >>> np.linalg.cond(mat)
+    4.976880305142543
+
+    >>> # 2) Generating a super small ill-conditioned random pentadiagonal matrix
+    >>> mat = gen_conditioned_rand_penta_matrix_dense(
+    ...     n_rows=n_rows,
+    ...     seed=seed,
+    ...     ill_conditioned=True,
+    ... )
+    >>> mat
+    array([[ 0.92453713,  0.28308514, -0.09972199],
+           [-0.09784268,  0.2270634 , -0.1509019 ],
+           [-0.23431267,  0.00468463, -0.02273771]])
+    >>> # its bandwidth is computed and should be equal to 2
+    >>> bandwidth(mat)
+    (2, 2)
+    >>> # its condition number is computed and its value should be close to the
+    >>> # reciprocal floating point precision, i.e., ~1e16
+    >>> np.linalg.cond(mat)
+    1.493156437173682e+17
+
+    >>> # 3) Generating a small well-conditioned random pentadiagonal matrix
+    >>> n_rows = 7
+
+    >>> mat = gen_conditioned_rand_penta_matrix_dense(
+    ...     n_rows=n_rows,
+    ...     seed=seed,
+    ...     ill_conditioned=False,
+    ... )
+    >>> np.round(mat, 2)
+    array([[ 0.92, -0.72,  0.73,  0.  ,  0.  ,  0.  ,  0.  ],
+           [ 0.83, -0.02,  1.08,  0.41,  0.  ,  0.  ,  0.  ],
+           [-0.58,  0.13, -0.13, -0.37,  0.18,  0.  ,  0.  ],
+           [ 0.  , -0.07, -0.58,  0.46, -0.31,  0.28,  0.  ],
+           [ 0.  ,  0.  ,  0.43,  0.13,  0.39, -0.1 , -0.15],
+           [ 0.  ,  0.  ,  0.  ,  0.06, -0.14,  0.4 ,  0.28],
+           [ 0.  ,  0.  ,  0.  ,  0.  , -0.14,  0.36,  0.53]])
+    >>> # its bandwidth is computed and should be equal to 2
+    >>> bandwidth(mat)
+    (2, 2)
+    >>> # its condition number is computed and values below 1e10 can be considered good
+    >>> np.linalg.cond(mat)
+    42.4847446467131
+
+    >>> # 4) Generating a small ill-conditioned random pentadiagonal matrix
+    >>> mat = gen_conditioned_rand_penta_matrix_dense(
+    ...     n_rows=n_rows,
+    ...     seed=seed,
+    ...     ill_conditioned=True,
+    ... )
+    >>> np.round(mat, 2)
+    array([[ 0.92, -0.72,  0.73,  0.  ,  0.  ,  0.  ,  0.  ],
+           [ 0.83, -0.02,  1.08,  0.41,  0.  ,  0.  ,  0.  ],
+           [-0.58,  0.13, -0.13, -0.37,  0.18,  0.  ,  0.  ],
+           [ 0.  , -0.07, -0.58,  0.46, -0.31,  0.28,  0.  ],
+           [ 0.  ,  0.  ,  0.43,  0.13,  0.39, -0.1 , -0.15],
+           [ 0.  ,  0.  ,  0.  ,  0.06, -0.14,  0.4 ,  0.28],
+           [ 0.  ,  0.  ,  0.  ,  0.  , -0.14,  0.36,  0.28]])
+    >>> # its bandwidth is computed and should be equal to 2
+    >>> bandwidth(mat)
+    (2, 2)
+    >>> # its condition number is computed and its value should be close to the
+    >>> # reciprocal floating point precision, i.e., ~1e16
+    >>> np.linalg.cond(mat)
+    1.1079218802103074e+17
+
+    >>> # 5) Generating a large well-conditioned random pentadiagonal matrix
+    >>> n_rows = 1_000
+
+    >>> mat = gen_conditioned_rand_penta_matrix_dense(
+    ...     n_rows=n_rows,
+    ...     seed=seed,
+    ...     ill_conditioned=False,
+    ... )
+    >>> # its bandwidth is computed and should be equal to 2
+    >>> bandwidth(mat)
+    (2, 2)
+    >>> # its condition number is computed and values below 1e10 can be considered good
+    >>> np.linalg.cond(mat)
+    9570.995402466417
+
+    >>> # 6) Generating a large ill-conditioned random pentadiagonal matrix
+    >>> mat = gen_conditioned_rand_penta_matrix_dense(
+    ...     n_rows=n_rows,
+    ...     seed=seed,
+    ...     ill_conditioned=True,
+    ... )
+    >>> # its bandwidth is computed and should be equal to 2
+    >>> bandwidth(mat)
+    (2, 2)
+    >>> # its condition number is computed and its value should be close to the
+    >>> # reciprocal floating point precision, i.e., ~1e16
+    >>> np.linalg.cond(mat)
+    5.058722571393928e+17
+
+    """
+
+    # first, the fake diagonal matrix is generated whose entries are strictly
+    # positive and sorted in descending order
+    np.random.seed(seed=seed)
+    d_diag = np.flip(np.sort(np.random.rand(n_rows)))
+
+    # the conditioning is achieved by manipulating the smallest diagonal entry
+    # Case 1: well-conditioned matrix
+    if not ill_conditioned:
+        # here, the smallest diagonal entry is set to a value that is enforced to have
+        # a minimum magnitude
+        d_diag = np.maximum(d_diag, _MIN_DIAG_VAL)
+
+    # Case 2: ill-conditioned matrix
+    else:
+        # here, the smallest diagonal entry is set to a value that is numerically zero
+        # compared to the largest entry
+        d_diag[n_rows - 1] = 0.1 * np.finfo(np.float64).eps * d_diag[0]
+
+    # ... followed by a unit lower triangular matrix with 2 sub-diagonals, but here
+    # the entries may be negative ...
+    diagonals = [
+        1.0 - 2.0 * np.random.rand(n_rows - 2),
+        1.0 - 2.0 * np.random.rand(n_rows - 1),
+        np.ones(n_rows),
+    ]
+    l_mat = sprs.diags(
+        diagonals=diagonals,
+        offsets=[-2, -1, 0],  # type: ignore
+        shape=(n_rows, n_rows),
+        format="csr",
+        dtype=np.float64,
+    )
+
+    # ... and an upper triangular matrix with 2 super-diagonals
+    diagonals = [
+        np.ones(n_rows),
+        1.0 - 2.0 * np.random.rand(n_rows - 1),
+        1.0 - 2.0 * np.random.rand(n_rows - 2),
+    ]
+    u_mat = sprs.diags(
+        diagonals=diagonals,
+        offsets=[0, 1, 2],  # type: ignore
+        shape=(n_rows, n_rows),
+        format="csr",
+        dtype=np.float64,
+    )
+
+    # finally, the matrix is reconstructed by multiplying the three matrices
+    return (l_mat.multiply(d_diag[np.newaxis, ::]).dot(u_mat)).toarray()
 
 
 # === Doctests ===
