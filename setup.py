b93102baba32f5d1d14282ac8f7dddf76cf3572d
MothNik <nik.zoe@web.de>
2024-06-09 18:32:03 +0200

    package: [11] included build information for parallelized solvers

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
 """pentapy: A toolbox for pentadiagonal matrices."""
 
 import os
+import sys
 
 import Cython.Compiler.Options
 import numpy as np
 from Cython.Build import cythonize
 from setuptools import Extension, setup
+
+if sys.platform.startswith("win"):
+    openmp_arg = "/openmp"
+else:
+    openmp_arg = "-fopenmp"
 
 Cython.Compiler.Options.annotate = True
 
@@ -16,6 +22,8 @@ CY_MODULES = [
         sources=[os.path.join("src", "pentapy", "solver.pyx")],
         include_dirs=[np.get_include()],
         define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
+        extra_compile_args=[openmp_arg],
+        extra_link_args=[openmp_arg],
     )
 ]
 
